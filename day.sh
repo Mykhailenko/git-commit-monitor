e0fdfbb8ce039e91f253b9752bf3c8d4a551ffc3
Mykhailenko <hlib.mykhailenko@gmail.com>
2019-07-04 12:11:26 +0200

    Handle day range

--- day.sh
+++ day.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
 # chech paramteres
-if [ "$#" -ne 1 ]; then
-    echo "Please provide one argument: date in YYYY-MM-DD format, e.g. 2019-07-03"
+
+
+if [ "$#" -gt 2 ] || [ "$#" -eq 0 ] ; then
+    echo "Example of usage: ./day 2019-07-01 [2019-07-03]"
     exit
 fi
+
+from=$1
+till=$1
+fromtill=$from
+reportName=day-$1
+
+if [ "$#" -eq 2 ]; then
+   till=$2
+   fromtill="$from - $till"
+   reportName=day-$1-$2
+fi
+
 
 # read list.txt file into array 'repos'
 IFS=$'\n' read -d '' -r -a repos < ./list.txt
 
-reportName=day-$1
 
 rm -f ./$reportName
 
-echo    "===========================================" >> ./$reportName
-echo    "=== Day report for the date: $1 ===" >> ./$reportName
-echo -e "===========================================\n" >> ./$reportName
+echo -e "=== Day report for the date: $fromtill ===\n" >> ./$reportName
 
 # going through all repositories
 for repo in "${repos[@]}"
@@ -32,7 +43,7 @@ do
   (cd repos/$name; git pull origin master >/dev/null 2>&1)
 
   # apply git log for the given date
-  output=$(cd repos/$name; git log  --pretty=format:"%h - %an : %s" --since="$1 00:00" --until="$1 23:59")
+  output=$(cd repos/$name; git log  --pretty=format:"%h - %an : %s" --since="$from 00:00" --until="$till 23:59")
 
   # if there is something in 'output' then write it to the file
   if [[ $output ]]; then
